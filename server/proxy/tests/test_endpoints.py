4235bbb25e4d534829bd486f601cf8c46371b98c
Benoit Alessandroni <benoit@happy-dev.fr>
2023-03-12 19:03:35 +0100

    update: renaming directories

--- server/proxy/tests/test_endpoints.py
+++ server/proxy/tests/test_endpoints.py
@@ -10,14 +10,14 @@ class JSONToFileTests(TestCase):
         # Test saving a JSON file with a valid filename and data
         filename = 'test_file.json'
         data = {'message': 'Hello, world!'}
-        response = self.client.post('/json-to-file/{}'.format(filename), json.dumps(data), content_type='application/json')
+        response = self.client.post('/json-to-file/{}/'.format(filename), json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_save_json_file_error(self):
         # Test saving a JSON file with an invalid filename
         filename = 'test_file.pdf'
         data = {'message': 'Hello, world!'}
-        response = self.client.post('/json-to-file/{}'.format(filename), json.dumps(data), content_type='application/json')
+        response = self.client.post('/json-to-file/{}/'.format(filename), json.dumps(data), content_type='application/pdf')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
@@ -28,12 +28,12 @@ class FileToJSONTests(TestCase):
     def test_load_json_file_success(self):
         # Test loading a JSON file with a valid filename
         filename = 'test_file.json'
-        response = self.client.get('/file-to-json/?filename={}'.format(filename))
+        response = self.client.get('/file-to-json/{}/'.format(filename))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(json.loads(response.content), {'message': 'Hello, world!'})
 
     def test_load_json_file_error(self):
         # Test loading a non-existent JSON file
         filename = 'nonexistent_file.json'
-        response = self.client.get('/file-to-json/?filename={}'.format(filename))
+        response = self.client.get('/file-to-json/{}/'.format(filename))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
